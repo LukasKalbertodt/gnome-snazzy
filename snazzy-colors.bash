c3ce638ae4eb959d2eae17c4980eb6d28eaa7de0
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-06-25 10:21:53 +0200

    Fix name of scheme

--- snazzy-colors.bash
+++ snazzy-colors.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Base16 - Gnome Terminal color scheme install script
 
-[[ -z "$PROFILE_NAME" ]] && PROFILE_NAME="terminal.sexy"
-[[ -z "$PROFILE_SLUG" ]] && PROFILE_SLUG="terminal-dot-sexy"
+[[ -z "$PROFILE_NAME" ]] && PROFILE_NAME="snazzy"
+[[ -z "$PROFILE_SLUG" ]] && PROFILE_SLUG="snazzy"
 [[ -z "$DCONF" ]] && DCONF=dconf
 [[ -z "$UUIDGEN" ]] && UUIDGEN=uuidgen
 
